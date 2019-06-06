5ac4b2bf3451b652f78195be0947f861eb57adcf
groundziro <thomasjosse92@gmail.com>
2019-06-06 11:40:15 +0200

    Début Rapport + Avancement Script R Question 1^

--- Projet/Projet.R
+++ Projet/Projet.R
@@ -1,5 +1,10 @@
 install.packages("xlsx")
+library(xlsx)
 table = read.xlsx("../pourcentage_chercheurs_belgique.xlsx", sheetIndex = 1)
 rownames(table) = table$geo.time
 table = table[,-1]
 boxplot(table)
+boxplot(scale(table,center = T,scale = T))
+cor_table = cor(table)
+table.pca = dudi.pca(df = table, scannf = FALSE, nf = 1)
+s.corcircle(table.pca$tab)
