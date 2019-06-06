dbe9a155ad705a7f4b30d45d035a43cc33107a71
groundziro <thomasjosse92@gmail.com>
2019-06-06 14:11:13 +0200

    Script R projet quasi fini (reste les plots à mettre dans des pdfs) + début Rapport Question 1 et début présentation pour ADL

--- Projet/Projet.R
+++ Projet/Projet.R
@@ -1,3 +1,4 @@
+#ACP
 install.packages("xlsx")
 library(xlsx)
 table = read.xlsx("../pourcentage_chercheurs_belgique.xlsx", sheetIndex = 1)
@@ -8,3 +9,20 @@ boxplot(scale(table,center = T,scale = T))
 cor_table = cor(table)
 table.pca = dudi.pca(df = table, scannf = FALSE, nf = 1)
 s.corcircle(table.pca$tab)
+
+#Classification
+
+dist = dist(table)
+ward = hclust(dist,method = "ward.D")
+plot(ward,main="Méthode Ward")
+rect.hclust(ward, border = c("green","red"),h = 1)
+single = hclust(dist,method = 'single')
+plot(single,main="Méthode single")
+rect.hclust(single,border = c("green","red"),h = 1)
+complete = hclust(dist,method = 'complete')
+plot(complete, main = "Methode Complète")
+rect.hclust(complete,border = c("green","red"),h = 1)
+plot(dist)
+plot(ward)
+s.class(table)
+cutree(ward,h = 2)
